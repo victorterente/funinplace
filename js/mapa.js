c2615983649dad654437bb2dc668b7620df307bf
Duarte de Noronha Forte Correia <duarte.noronha.correia@gmail.com>
2022-05-18 18:09:13 +0100

    ccc

--- js/mapa.js
+++ js/mapa.js
@@ -154,81 +154,75 @@ let map = new google.maps.Map(document.getElementById('map'), mapOptions);
   
   console.log(markers);
   
-  function filterDiscotecas()
+  /*function filterDiscotecas()
   {
-    markers.forEach((marker) => {
-        marker.setMap(null);
-      });
-      markers = [];
-      getDisco();
+
   }
-  document.getElementById("Discoteca/Bar").onclick = filterDiscotecas;
+  document.getElementById("Discoteca/Bar").onclick = filterDiscotecas;*/
 
 
-  //barra pesquisa
-  var input = document.getElementById('searchInput');
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+   //GEo Coding search bar
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
+   var input = document.getElementById('pac-input');
+   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  var infowindow1 = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-      map: map,
-      anchorPoint: new google.maps.Point(0, -29)
-  });
+   var autocomplete = new google.maps.places.Autocomplete(input);
+   autocomplete.bindTo('bounds', map);
 
-  autocomplete.addListener('place_changed', function() {
-      infowindow1.close();
-      marker.setVisible(false);
-      var place = autocomplete.getPlace();
-      if (!place.geometry) {
-          window.alert("Autocomplete's returned place contains no geometry");
-          return;
-      }
+   var infowindow1 = new google.maps.InfoWindow();
+   var marker1 = new google.maps.Marker({
+       map: map,
+       anchorPoint: new google.maps.Point(0, -29)
+   });
 
-      // If the place has a geometry, then present it on a map.
-      if (place.geometry.viewport) {
-          map.fitBounds(place.geometry.viewport);
-      } else {
-          map.setCenter(place.geometry.location);
-          map.setZoom(17);
-      }
-      marker.setIcon(({
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(35, 35)
-      }));
-      marker.setPosition(place.geometry.location);
-      marker.setVisible(true);
-  
-      var address = '';
-      if (place.address_components) {
-          address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || ''),
-            (place.address_components[2] && place.address_components[2].short_name || '')
-          ].join(' ');
-      }
-  
-      infowindow1.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-      infowindow1.open(map, marker);
-    
-      // Location details
-      for (var i = 0; i < place.address_components.length; i++) {
-          if(place.address_components[i].types[0] == 'postal_code'){
-              document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
-          }
-          if(place.address_components[i].types[0] == 'country'){
-              document.getElementById('country').innerHTML = place.address_components[i].long_name;
-          }
-      }
-      document.getElementById('location').innerHTML = place.formatted_address;
-      document.getElementById('lat').innerHTML = place.geometry.location.lat();
-      document.getElementById('lon').innerHTML = place.geometry.location.lng();
-  });
+   autocomplete.addListener ('place_changed', function() {
+     infowindow1.close();
+     marker1.setVisible(false);
+     var place = autocomplete.getPlace();
+     if (!place.geometry) {
+         window.alert("Autocomplete's returned place contains no geometry");
+         return;
+     }
+
+     // If the place has a geometry, then present it on a map.
+     if (place.geometry.viewport) {
+         map.fitBounds(place.geometry.viewport);
+     } else {
+         map.setCenter(place.geometry.location);
+         map.setZoom(17);
+     }
+     marker1.setIcon(({
+         url: place.icon,
+         size: new google.maps.Size(71, 71),
+         origin: new google.maps.Point(0, 0),
+         anchor: new google.maps.Point(17, 34),
+         scaledSize: new google.maps.Size(35, 35)
+     }));
+     marker1.setPosition(place.geometry.location);
+     marker1.setVisible(true);
+
+     var address = '';
+     if (place.address_components) {
+         address = [
+             (place.address_components[0] && place.address_components[0].short_name || ''),
+             (place.address_components[1] && place.address_components[1].short_name || ''),
+             (place.address_components[2] && place.address_components[2].short_name || '')
+         ].join(' ');
+     }
+
+     infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+     infowindow.open(map, marker1);
+
+     // Location details
+     for (var i = 0; i < place.address_components.length; i++) {
+         if(place.address_components[i].types[0] == 'postal_code'){
+             document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+         }
+         if(place.address_components[i].types[0] == 'country'){
+             document.getElementById('country').innerHTML = place.address_components[i].long_name;
+         }
+     }
+ });
 
 }
 window.initMap = initMap;
